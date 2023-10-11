08a338b5ccea1194ee22404111583b6e0ec3efc2
BreElle Wells <breellejordyn@gmail.com>
2023-10-11 10:45:24 -0600

    render game data

--- client/src/components/pages/Game.js
+++ client/src/components/pages/Game.js
@@ -14,24 +14,41 @@ function Game() {
         })
     }, [])
 
+    const game_developers = game.developers.map((developer) => (
+        <p>{developer.name}</p>
+    ))
+
+    const game_genres = game.genres.map((genre) => (
+        <span>{genre.name}</span>
+    ))
+
+    const game_platforms = game.platforms.map((platform) => (
+        <span>{platform.platform.name}</span>
+    ))
+
+    const game_publishers = game.publishers.map((publisher) => (
+        <span>{publisher.name}</span>
+    ))
+
+    const game_stores = game.stores.map((store) => (
+        <span>{store.store.name}</span>
+    ))
+
     return (
         <>
             This page will display a single game's information. You will need the id of this game to be the id that your API uses so you can make a fetch. It's making a request with your database's id right now.
+            <p>{game.name}</p>
             <img src={game.background_image} />
             <img src={game.background_image_additional} />
-            <p>{game.description}</p>
-            <p>{game.description_raw}</p>
-            {/* <p>{game.developers}</p>
-            <p>{game.genres}</p>
-            <p>{game.name}</p>
-            <p>{game.platforms}</p>
-            <p>{game.publishers}</p>
+            <p dangerouslySetInnerHTML={{__html: game.description}}></p>
+            {game_developers}
+            {game_genres}
+            {game_platforms}
+            {game_publishers}
             <p>{game.released}</p>
             <p>{game.id}</p>
-            <p>{game.alternative_names}</p>
             <p>{game.esrb_rating}</p>
-            <p>{game.slug}</p>
-            <p>{game.stores}</p> */}
+            {game_stores}
         </>
     )
 }
