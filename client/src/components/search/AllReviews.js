08369d1bffa36359abca6acd3eb3cc1585c41ddf
BreElle Wells <breellejordyn@gmail.com>
2023-10-13 15:09:32 -0600

    begin review format and filter capabilities

--- client/src/components/search/AllReviews.js
+++ client/src/components/search/AllReviews.js
@@ -1,62 +1,88 @@
 import React, {useState, useEffect} from "react";
+import { NavLink } from "react-router-dom";
 import ReviewPreview from "../previews/ReviewPreview";
 
 function AllReviews() {
     const [reviews, setReviews] = useState([])
-    const [sort, setSort] = useState("")
+    const [platforms, setPlatforms] = useState()
     
     useEffect(() => {
         fetch("/all-reviews")
-        .then(r => {
+        .then((r) => {
             if (r.ok) {
                 r.json().then((reviews) => setReviews(reviews))
             }
         })
     }, [])
 
-    // let rendered_reviews = reviews.map((review) => (
-    //         <ReviewPreview
-    //         key={review.id}
-    //         {...review}
-    //         />
-    //     ))
 
-    function submitFilterChoice(e) {
-        e.preventDefault()
-        setSort(e.target.value)
+    function fetchPlatforms() {
+        fetch("/platforms")
+        .then((r) => {
+            if (r.ok) {
+                r.json().then((platforms) => setPlatforms(platforms))
+            }
+        })
     }
 
-    let unorganized_reviews = reviews.map((review) => (<ReviewPreview key={review.id} {...review}/>))
+    if (!platforms) fetchPlatforms()
+
+    let rendered_reviews = reviews.map((review) => (
+        <tr>
+                <td>{review.game_name}</td>
+                <td>{review.platform}</td>
+                <td>{review.rating} / 5</td>
+                <NavLink to={`/reviews/${review.id}`}>
+                    <td>{`${review.review.substring(0, 50)}...`}</td>
+                </NavLink>
+        </tr>
+    ))
+
     
-    let rendered_reviews
-
-    // WON'T GO BACK TO THE NORMAL, UNORGANIZED VERSION
-
-    if (sort === "") {
-        rendered_reviews = unorganized_reviews
-    } else if (sort === "game_name") {
-        const sortedByGame = reviews.sort((a,b) => a.game_name.localeCompare(b.game_name))
-        rendered_reviews = sortedByGame.map(((review) => (<ReviewPreview key={review.id} {...review}/>)))
-    } else if (sort === "rating") {
-        const sortedByRating = reviews.sort((a,b) => a.rating - b.rating)
-        rendered_reviews = sortedByRating.map(((review) => (<ReviewPreview key={review.id} {...review}/>)))
-    } else if (sort === "platform") {
-        const sortedByPlatform = reviews.sort((a,b) => a.platform.localeCompare(b.platform))
-        rendered_reviews = sortedByPlatform.map(((review) => (<ReviewPreview key={review.id} {...review}/>)))
+    if (!platforms) {
+        return <p>Loading...</p>
     }
+
+    let rendered_platforms = platforms.results.map((platform) => (
+        <li><a>{platform.name}</a></li>
+    ))
     
     return (
         <>
             <h1>All Reviews</h1>
-            <label>Sort by:</label>
-            <select onChange={(e) => submitFilterChoice(e)}>
-                <option value=""></option>
-                <option value="game_name">Game</option>
-                <option value="rating">Rating</option>
-                <option value="platform">Platform</option>
-                {/* <option>Player</option> */}
-            </select>
-            {rendered_reviews}
+            <div className="dropdown dropdown-hover">
+                <label tabIndex={0} className="btn m-1">Filter by Rating</label>
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a>0+</a></li>
+                    <li><a>1+</a></li>
+                    <li><a>2+</a></li>
+                    <li><a>3+</a></li>
+                    <li><a>4+</a></li>
+                </ul>
+            </div>
+
+            <div className="dropdown dropdown-hover">
+                <label tabIndex={0} className="btn m-1">Filter by Platform</label>
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                    {rendered_platforms}
+                </ul>
+            </div>
+            
+            <div className="overflow-x-auto">
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>Game</th>
+                        <th>Rating</th>
+                        <th>Platform</th>
+                        <th>Review</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {rendered_reviews}
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
